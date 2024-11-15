a3d6da59fdeb1ff5f7c89d78134160252527e472
Hector Castro <goldenrage404@gmail.com>
2024-11-14 22:34:05 -0700

    Add files via upload

---
+++ NombreDeCompañeros.java
@@ -0,0 +1,20 @@
+public static void main(String[] args) {
+    // Crear un arreglo de 5 elementos de tipo String
+    // Almacena el nombre de 5 de tus compañeros que si asistieron hoy
+    String[] nombres = new String[5];
+    nombres[0] = "Fernando Zamudio";
+    nombres[1] = "Hector Castro";
+    // nombres[2] = "";
+    nombres[3] = "Daniel Chaidez";
+    nombres[4] = "Ximena Reyes";
+    
+    /* nombres
+     * 0 / Fernando Zamudio /
+     * 1 / Hector Castro /
+     * 2 / /
+     * 3 / Daniel Chaidez /
+     * 4 / Ximena Reyes /
+     */
+    
+    System.out.println(nombres);
+}
