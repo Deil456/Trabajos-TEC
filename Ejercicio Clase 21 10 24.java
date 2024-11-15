3e87233baeb6a56dc3bba8bbb306a344e90d83cd
Hector Castro <goldenrage404@gmail.com>
2024-11-15 12:12:18 -0700

    Update Ejercicio Clase 21 10 24.java

--- Ejercicio Clase 21 10 24.java
+++ Ejercicio Clase 21 10 24.java
@@ -3,32 +3,27 @@ import java.util.Random;
 public class OrdenamientoBurbuja {
 
     public static void main(String[] args) {
-        int[] numeros = new int[100]; // Crear un arreglo de 100 elementos
+        int[] numeros = new int[100]; 
         Random random = new Random();
-
-        // Asignar valores aleatorios entre 500 y 1000 al arreglo
+       
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = 500 + random.nextInt(501); // Genera valores entre 500 y 1000
+            numeros[i] = 500 + random.nextInt(501); 
         }
 
-        // Mostrar el arreglo antes del ordenamiento
         System.out.println("Arreglo antes del ordenamiento:");
         mostrarArreglo(numeros);
 
-        // Ordenar el arreglo utilizando el método de burbuja
         ordenarBurbuja(numeros);
 
-        // Mostrar el arreglo después del ordenamiento
-        System.out.println("\nArreglo después del ordenamiento:");
+        System.out.println("\nArreglo despues del ordenamiento:");
         mostrarArreglo(numeros);
     }
 
-    // Método para ordenar un arreglo con el algoritmo de burbuja
     public static void ordenarBurbuja(int[] arreglo) {
         int n = arreglo.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                // Intercambiar si el elemento actual es mayor que el siguiente
+                
                 if (arreglo[j] > arreglo[j + 1]) {
                     int temp = arreglo[j];
                     arreglo[j] = arreglo[j + 1];
@@ -38,7 +33,6 @@ public class OrdenamientoBurbuja {
         }
     }
 
-    // Método para mostrar los elementos de un arreglo
     public static void mostrarArreglo(int[] arreglo) {
         for (int numero : arreglo) {
             System.out.print(numero + " ");
